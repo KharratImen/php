689f909e71cd6394c0512dc5b4fa10432e40ccc3
KharratImen <kharratimen04@gmail.com>
2024-02-27 08:18:51 +0100

    cntrol

--- ControlUser.php
+++ ControlUser.php
@@ -3,7 +3,7 @@ require_once 'User.php';
 if (isset($_POST['login']) && isset($_POST['password'])) {
     $login = $_POST['login'];
     $password = $_POST['password'];
-    $user = new User($login, $password);
+    $user = new $user($login, $password);
 } else {
     header("Location:authentification.html");
 }
